a598b4333fe365985f0f1640c6dc11589ef44db7
mikekravets <michkravets@gmail.com>
2024-04-26 10:12:26 +0300

    Modify points and conditions

--- oop/points.py
+++ oop/points.py
@@ -1,12 +1,30 @@
+from typing import Any
+
+
 class Point:
 
     def __init__(self, x: float, y: float) -> None:  # Constructor
         self.x = x
         self.y = y
+        self.furehfuiewhfuie = "fhuewheu"
+
+    def to_rad(self):
+        return self.x**0.5
+
+    def __str__(self) -> str:
+        return f"({self.x}, {self.y})"
+
+    def __call__(self) -> Any:
+        return self.x + self.y
 
 
-# class str:
-#     pass
+class RadPoint(Point):
+
+    def __init__(self, x: float) -> None:
+        super().__init__(x, 0)
+
+    def to_rad(self):
+        raise ValueError("Cannot call to_rad from radial point")
 
 
 class Triple:
@@ -23,6 +41,13 @@ def get_string(s: str):
     pass
 
 
+class Math:
+
+    @staticmethod
+    def custom_pow(x: int):
+        return x**2
+
+
 if __name__ == "__main__":
     x1, y1 = 0, 0
     x2, y2 = 0, 0
@@ -35,4 +60,12 @@ if __name__ == "__main__":
     p4 = (0, 4)
 
     p1 = Point(0, 1)
+    print(p1)
+    print(p1())
 
+    radp1 = RadPoint(1)
+    radp1.to_rad()
+    RadPoint.to_rad(radp1)
+    s = "string"
+
+    print(Math.custom_pow())
